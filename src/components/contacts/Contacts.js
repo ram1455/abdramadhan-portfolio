6164fa6c079a65e2bf418a79aa3ff374f19974da
Abdurrahman-Ramadhan-PT <puterateguh777@gmail.com>
2023-02-17 17:39:42 +0700

    "ada div tidak jelas"

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -9,7 +9,7 @@ export default function Contacts() {
   return (
     <div className='contacts' id='contacts'>
         <h1>CONTACT ME HERE!</h1>
-        <div div>
+        <div>
             <a  href="https://www.instagram.com/abdrhmn.rmdhn/" target={'_blank'} rel="noreferrer" ><img className='logo' src={iglogo} alt="" /></a>
             <a  href="https://wa.me/62895384791064" target={'_blank'} rel="noreferrer" ><img className='logo' src={walogo} alt="" /></a>
             <a  href="https://web.facebook.com/profile.php?id=100013336002306" target={'_blank'} rel="noreferrer" ><img className='logo' src={facebooklogo} alt="" /></a>
