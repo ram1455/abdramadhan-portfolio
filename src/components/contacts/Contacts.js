67cad4881bc3c4bba973d7180cdae939f23dd259
Abdurrahman-Ramadhan-PT <puterateguh777@gmail.com>
2023-02-17 17:39:16 +0700

    "add link ke contacts"

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -9,11 +9,11 @@ export default function Contacts() {
   return (
     <div className='contacts' id='contacts'>
         <h1>CONTACT ME HERE!</h1>
-        <div>
-            <a  href="" target={'_blank'} rel="noreferrer" ><img className='logo' src={iglogo} alt="" /></a>
-            <a  href="" target={'_blank'} rel="noreferrer" ><img className='logo' src={walogo} alt="" /></a>
-            <a  href="" target={'_blank'} rel="noreferrer" ><img className='logo' src={facebooklogo} alt="" /></a>
-            <a  href="" target={'_blank'} rel="noreferrer" ><img className='logo' src={githublogo} alt="" /></a>
+        <div div>
+            <a  href="https://www.instagram.com/abdrhmn.rmdhn/" target={'_blank'} rel="noreferrer" ><img className='logo' src={iglogo} alt="" /></a>
+            <a  href="https://wa.me/62895384791064" target={'_blank'} rel="noreferrer" ><img className='logo' src={walogo} alt="" /></a>
+            <a  href="https://web.facebook.com/profile.php?id=100013336002306" target={'_blank'} rel="noreferrer" ><img className='logo' src={facebooklogo} alt="" /></a>
+            <a  href="https://github.com/ram1455" target={'_blank'} rel="noreferrer" ><img className='logo' src={githublogo} alt="" /></a>
         </div>
     </div>
   )
